6740b6534457559fca57ba18139416245cc48679
Krzysiu2388 <85492188+Krzysiu2388@users.noreply.github.com>
2023-03-22 15:09:16 +0100

    Add files via upload

---
+++ silnik.js
@@ -0,0 +1,45 @@
+import kaboom from "https://unpkg.com/kaboom/dist/kaboom.mjs";
+
+// initialize kaboom context
+kaboom();
+
+// wczytywanie obrazkow
+
+loadSprite("tlo","choooinka.png")
+loadSprite("krolik","krolik.png")
+loadSound("muzyka","do szopy hej pasterze.mp3")
+
+
+onKeyRelease("space",()=>{
+    play("muzyka")
+})
+
+
+
+
+add([
+    sprite("tlo"),
+    pos(0,0)
+])
+
+const krolik = add([
+    sprite("krolik"),
+pos(100,300)
+])
+
+
+var xMinus =1;
+onUpdate(()=>{
+    if(krolik.pos.x ==100)
+    {
+         xMinus = 1;
+    }
+    if(krolik.pos.x == 400)
+    {
+    xMinus = -1;   
+    }   
+krolik.pos.x += xMinus;
+
+}
+)
+
